486f22d32c0f8b69c8b188b363a3b472d1264039
bananabite <72071891+bananabite@users.noreply.github.com>
2024-02-12 17:29:12 +1100

    data setup function class created
    function allowing parsing of PHIDU data into a machine readable format ready for input into ML. needs further optimisation

---
+++ Reworked/functions.py
@@ -0,0 +1,23 @@
+import pandas as pd
+#sheets = ['Education','Early_childhood_development','Families', 'Housing','IRSD', 'Mothers_babies']
+
+class setup_data():
+    def __init__(self, file, sheets):
+        self.file = file
+        self.sheets = sheets
+        self.data = self.create_data()
+
+    def init_sheet(self,sheet):
+        df = pd.read_excel(self.file, sheet_name = sheet)
+        df.columns = df.iloc[3]
+        df = df.iloc[4:596]
+        df = df.dropna(axis = 'columns')
+        df = df.rename(columns = {'Code\n(PHN/LGA)': 'LGA_code','Name\n(PHN/LGA)':'LGA_name'})
+        return df
+
+    def create_data(self):
+        data = pd.DataFrame()
+        for sheet in self.sheets:
+            data = pd.concat([data,self.init_sheet(sheet)], axis =1)
+        data = data.drop(columns=['Quality indicator*'])
+        return data
